13b90920f0cb02cf7b19d55f8d3bb06b942682ad
Fenish <sohretalhadev@outlook.com>
2024-10-23 18:45:52 +0300

    Update shell scripts

--- setup.sh
+++ setup.sh
@@ -4,3 +4,10 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
+
+source_dir=$(cat config/appconfig.json | jq -r '.source_dir')
+save_folder=$(cat config/appconfig.json | jq -r '.save_folder')
+captions_folder=$(cat config/appconfig.json | jq -r '.captions_folder')
+
+# Create the directories
+mkdir -p "$source_dir" "$save_folder" "$captions_folder"
